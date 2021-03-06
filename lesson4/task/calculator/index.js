d979c747bc311084173ca2356f788e14304883f1
gon4aruk <sashagonch27@gmail.com>
2021-03-06 14:20:44 +0200

    ES modules

--- lesson4/task/calculator/index.js
+++ lesson4/task/calculator/index.js
@@ -12,9 +12,7 @@ export const mult = (a, b) => {
 
 // экспортируйте calc как export по умолчанию
 
-const calc = expression => {
+export default (expression) => {
   console.log('implementation for calc');
   // ... implementation
 };
-
-export default calc;
--- lesson4/task/index.js
+++ lesson4/task/index.js
@@ -3,11 +3,11 @@
 // импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
 // импортируйте ф-цию printProfile из profile/index.js под таким же именем
 
-import './polyfills/array-flat.js';
-import './polyfills/array-flatMAP.js';
 import calcExpression, { sum, mult } from './calculator/index.js';
 import fetchUser from './profile/gateway.js';
 import { printProfile } from './profile/index.js';
+import './polyfills/array-flat.js';
+import './polyfills/array-flatMAP.js';
 
 const calcResult = calcExpression('1 + 2');
 const sumResult = sum(1, 2);
