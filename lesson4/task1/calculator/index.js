d190b333a8a271644675ccd6ce8a8dd944bc50bc
gon4aruk <sashagonch27@gmail.com>
2021-03-07 18:04:32 +0200

    es modules

--- lesson4/task1/calculator/index.js
+++ lesson4/task1/calculator/index.js
@@ -12,7 +12,7 @@ export const mult = (a, b) => {
 
 // экспортируйте calc как export по умолчанию
 
-export default (expression) => {
+export default expression => {
   console.log('implementation for calc');
   // ... implementation
 };
